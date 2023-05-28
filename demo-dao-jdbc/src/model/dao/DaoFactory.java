531c5e3818b6c446a6cf9641e469f8d3561655c4
Maria Fernanda Gonçalves <91507393+nandinhaaa@users.noreply.github.com>
2023-05-28 20:38:37 -0300

    Add files via upload

--- demo-dao-jdbc/src/model/dao/DaoFactory.java
+++ demo-dao-jdbc/src/model/dao/DaoFactory.java
@@ -1,12 +1,16 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
-public class DaoFactory { //método que retorna o tipo da interface
+public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC(DB.getConnection()); //mas internamente instancia uma implementacao
-		
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 }
