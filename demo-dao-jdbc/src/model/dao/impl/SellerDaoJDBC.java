88c560f702b720a4e79201eb0bde0b7c0b4c4680
Maria Fernanda Gonçalves <91507393+nandinhaaa@users.noreply.github.com>
2023-05-28 19:38:58 -0300

    Add files via upload

--- demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
+++ demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -96,6 +99,52 @@ public class SellerDaoJDBC implements SellerDao {
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st =null;
+		ResultSet rs= null;
+		try {
+			st = conn.prepareStatement(
+
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department " 
+					+ "	ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? " 
+					+ "	ORDER BY Name " );
+					
+					st.setInt(1, department.getId());
+					
+					rs = st.executeQuery();
+					//percorrer enq tiver um next 
+					List<Seller> list = new ArrayList<>();
+					//controlar a nao repeticao do departamento com o Map 
+					Map<Integer, Department> map = new HashMap<>();
+					//map vazio acima para guardar qqlq depart que eu instanciar
+					//cada vez que passar no while ele vai no map
+					
+					while (rs.next()) {
+						//testando se o departamento já existe  com o mesmo ID  
+						Department dep = map.get(rs.getInt("DepartmentId"));
+						
+						if (dep == null) {// se entrar aq significa que ele n existia ainda 
+							dep = instantiateDepartment(rs);
+							map.put(rs.getInt("DepartmentId"), dep);
+						} // faz com que um mesmo departamento seja instanciando para 2 ou mais vendedores sem criar mais Departamentos 
+						Seller obj = instantiateSeller(rs,dep);
+						list.add(obj); 
+			
+					}
+					return list;
+					
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 	}
 
